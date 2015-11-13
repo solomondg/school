b3d6645cca04436cfa12c4ec5076478c0e3b780c
Solomon Greenberg <solomongreenberg@gmail.com>
2015-11-13 08:47:21 -0800

    test

--- caesarshift.py
+++
@@ -1,6 +0,0 @@
-rot = int(input("Enter what ROT you want: "))
-toencrypt = input("Enter string to be encrypted: ")
-encrypted = []
-for x in toencrypt:
-    encrypted += chr(ord(x) + rot)
-print (''.join(encrypted))
